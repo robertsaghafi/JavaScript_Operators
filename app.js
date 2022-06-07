7c5267ebdfa8430ae7094dc5ff12114aba842fc9
Robert Saghafi <robert.saghafi@calybreventures.io>
2022-06-07 14:33:03 -0500

    Completed JS Operators Exercise

--- app.js
+++ app.js
@@ -6,14 +6,58 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+let a = 20;
+let b = 4;
+let minus = (a-b);
+let multiply = (a*b);
+let dividing = (a/b);
+console.log(multiply); // 80
+console.log(dividing); // 5
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+var a1 = (num + str); 
+console.log(a1); // 1111
+
+var a2 = (num + str2);
+console.log(a2); // 11eleven
+
+var a3 = (num + isPresent);
+console.log(a3); // 12
+
+var a4 = (firstName + num);
+console.log(a4); // frodo11
+
+var a5 = (isPresent + str);
+console.log(a5); // true11
+
+var a6 = (firstName + lastName);
+console.log(a6); // FrodoBaggins
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
-// YOUR CODE HERE
+let val = 5;
+let str = "5";
+let str2 = "five";
+let isPresent = false;
+
+
+
+- What is the value of: val == str? // True
+- What is the value of: val === str? // False
+- What is the value of: !isPresent? // True
+- What is the value of: (“eleven” == str2 && val >= str)? // False
+- What is the value of: (!isPresent || isPresent)? // True
+- What is the value of: 0 == false? // True
+- What is the value of: 0 === false? // False
+- What is the value of: 0 != false? // False
+- What is the value of 0 !== false? // True
